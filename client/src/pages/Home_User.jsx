d6566446701907043b5dc128b61b3551bd652627
Santino2005 <santinocolombo13@gmail.com>
2025-04-08 11:26:44 -0300

    Added Home_User

--- client/src/pages/Home_User.jsx
+++ client/src/pages/Home_User.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 import ToggleDownButton from "../components/Buttons_and_others/ToggleDownButton.jsx";
+import {Outlet} from "react-router-dom";
+import styles from "./Home_User.module.css";
 
-function toggle_down_button(){
-    const button_down = document.getElementById("button_down");
-    button_down.classList.toggle("show");
-}
-
-window.onclick = function(event) {
-
-    if(!event.target.matches(".account_button")){
-        const toggle_down = document.getElementsByClassName("drop_content");
-        for(let i = 0; i < toggle_down.length; i++){
-            let open_drop = toggle_down[i];
-            if(open_drop.classList.contains("show")){
-                open_drop.classList.remove("show");
-            }
-        }
-    }
+function HomeUser(){
     return (
-        <div>
-            <ToggleDownButton/>
+        <div className={styles.screen}>
+            <nav className = {styles.navbar}>
+                <span className= {styles.logo}>
+                    <img src='/Logo.svg' alt='logo'/>
+                </span>
+                <ToggleDownButton />
+            {/* enlace a las paginas <a ref = > */}
+            </nav>
+            <main className={styles.content}>
+                <Outlet />
+            </main>
         </div>
-    );
+
+    )
 }
 
-export default toggle_down_button;
+export default HomeUser;
 
