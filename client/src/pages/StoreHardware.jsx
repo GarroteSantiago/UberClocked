6530a7bddd5a833fea3e76c9ca4a435b0b3f5129
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-04-20 17:17:29 -0300

    Page: Fixed the StoreHardware Page

--- client/src/pages/StoreHardware.jsx
+++ client/src/pages/StoreHardware.jsx
@@ -3,35 +3,18 @@ import styles from "./StoreHardware.module.css";
 import Navbar from "../components/navBar/Navbar.jsx";
 import Stars from "../components/Buttons_and_others/Stars.jsx";
 import ProductCard from "../components/store/ProductCard.jsx";
+import ProductCarousel from "../components/store/ProductCarousel.jsx";
 
 
 function StoreHardware({loggedIn, products}) {
-    const sliderRef = useRef(null);
-
-    const scroll = (direction) => {
-        if (sliderRef.current) {
-            sliderRef.current.scrollBy({
-                left: direction === "left" ? -300 : 300,
-                behavior: "smooth",
-            });
-        }
-    };
-
     return (
         <div className={styles.screen}>
             <Navbar loggedIn={loggedIn} onScreenUrl={"/store/pc-hardware"}/>
-
-            <div className={styles.shopContainer}>
-                <div className={styles.carouselControls}>
-                    <button className={styles.scrollButton} onClick={() => scroll("left")}>◀</button>
-                    <div className={styles.slider} ref={sliderRef}>
-                        {products.map((product) => (
-                            <ProductCard product={product}/>
-                        ))}
-                    </div>
-                    <button className={styles.scrollButton} onClick={() => scroll("right")}>▶</button>
-                </div>
-            </div>
+            <ProductCarousel>
+                {products.map((product) => (
+                    <ProductCard product={product}/>
+                ))}
+            </ProductCarousel>
         </div>
     );
 }
