aeb81e5fbeffc363532483c557884e1dd1db7a1b
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-04-28 17:23:04 -0300

    .

--- server/db/UberClockedDB.js
+++ server/db/UberClockedDB.js
@@ -1,17 +1,21 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
-dotenv.config();
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT
 });
 
-module.exports = db;
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la DB:', err.stack);
+        return;
+    }
+    console.log('Conectado a la DB como id ' + connection.threadId);
+});
+
+module.exports = connection;
