8683957a3ba7a39e3461539bb53ef8c7f8acf968
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-04-29 09:29:32 -0300

    .

--- client/src/components/Buttons_and_others/DeleteAccountButton.jsx
+++ client/src/components/Buttons_and_others/DeleteAccountButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './DeleteAccountButton.module.css';
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function DeleteAccountButton() {
+
+    const navigate = useNavigate();
     const handleDeleteAccount = async () => {
         const userId = localStorage.getItem('user_id');
+
         if (!userId) {
             console.error('No user ID found in localStorage');
             return;
@@ -13,10 +17,14 @@ function DeleteAccountButton() {
         try {
             const response = await axios.delete(`http://localhost:5000/api/users/${userId}`);
             console.log('Deleted:', response.data);
+            localStorage.clear();
+            // Redirect to login
+            navigate('/sign-up');
         } catch (error) {
             console.error('Error deleting user:', error);
         }
     };
+
 
     return (
         <button onClick={handleDeleteAccount} className={style.DeleteButton}>Delete your account</button>
